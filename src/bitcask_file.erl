2fa9d4b0bb252b09c00f48bf926d424bb4307700
Raghav Karol <rkarol@basho.com>
2017-03-02 09:18:19 +0100

    Fix dialyzer warnings on OTP-19
    In OTP-19 record field types are no longer automatically unioned with
    `undefined`.

--- src/bitcask_file.erl
+++ src/bitcask_file.erl
@@ -48,8 +48,8 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--record(state, {fd    :: file:fd(),
-                owner :: pid()}).
+-record(state, {fd    :: 'undefined' | file:fd(),
+                owner :: 'undefined' | pid()}).
 
 %%%===================================================================
 %%% API
